401b2c9685fe9f2e09d38f53a04cced8461e0042
Takhir <guvalakat1603@gmail.com>
2025-08-04 17:48:22 +0500

    Permission

---
+++ src/main/java/uz/pdp/config/security/Permission.java
@@ -0,0 +1,6 @@
+package uz.pdp.config.security;
+
+public class Permission {
+    public static final String CREATE_USER = "CREATE_USER";
+    public static final String BLOCK_USER = "BLOCK_USER_V2";
+}
