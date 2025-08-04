836188a1a018ef1ecd7755b9f9f0713423daced4
Takhir <guvalakat1603@gmail.com>
2025-08-04 17:49:40 +0500

    CustomUserDetailsService

--- src/main/java/uz/pdp/config/security/CustomUserDetailsService.java
+++ src/main/java/uz/pdp/config/security/CustomUserDetailsService.java
@@ -1,28 +1,31 @@
 package uz.pdp.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import uz.pdp.entity.AuthPermission;
+import uz.pdp.entity.AuthRole;
 import uz.pdp.entity.AuthUser;
+import uz.pdp.repository.AuthPermissionDao;
+import uz.pdp.repository.AuthRoleDao;
 import uz.pdp.repository.AuthUserDao;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final AuthUserDao authUserDao;
+    private final AuthRoleDao authRoleDao;
+    private final AuthPermissionDao authPermissionDao;
 
     @Autowired
-    public CustomUserDetailsService(AuthUserDao authUserDao) {
+    public CustomUserDetailsService(AuthUserDao authUserDao, AuthRoleDao authRoleDao, AuthPermissionDao authPermissionDao) {
         this.authUserDao = authUserDao;
+        this.authRoleDao = authRoleDao;
+        this.authPermissionDao = authPermissionDao;
     }
 
     @Override
@@ -30,9 +33,14 @@ public class CustomUserDetailsService implements UserDetailsService {
         AuthUser authUser = authUserDao.findByUsername(username)
                 .orElseThrow(
                         () -> new UsernameNotFoundException("User not found with username: " + username));
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_"+authUser.getRole()));
-        return new User(authUser.getUsername(), authUser.getPassword(),authorities);
+
+        List<AuthRole> userRoles = authRoleDao.findRolesByUserId(authUser.getId());
+        for (AuthRole role : userRoles) {
+            List<AuthPermission> permissionList = authPermissionDao.findPermissionsByRoleId(role.getId());
+            role.setPermissions(permissionList);
+        }
+        authUser.setRoles(userRoles);
+        return new CustomUserDetails(authUser);
     }
 
 }
